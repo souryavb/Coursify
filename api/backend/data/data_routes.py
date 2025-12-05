3c5454f557740dee925f882dc160e81752223065
KeltcherNeil <keltcher.n@northeastern.edu>
2025-12-05 11:20:55 -0500

    Updated data_routes with more

--- api/backend/data/data_routes.py
+++ api/backend/data/data_routes.py
@@ -21,8 +21,84 @@ FROM Professor AS p
 JOIN Rating AS r ON p.profID = r.profID
 GROUP BY p.profID, p.first_name, p.last_name, p.department;
 '''
+
     cursor.execute(the_query)
     theData = cursor.fetchall()
-    cursor.close()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype='application/json'
+    return the_response
+
+
+
+@data.route("/data/student", methods=["GET"])
+def get_student():
     
-    return jsonify(theData) 
+    cursor = db.get_db().cursor()
+    the_query= '''
+SELECT
+   c.courseID,
+   c.course_name,
+   s.CRN,
+   s.section_num,
+   AVG(e.grade) as average_grade
+FROM Section s
+INNER JOIN Course c ON s.CRN = c.courseID
+LEFT JOIN Enrollment e ON s.CRN = e.CRN
+GROUP BY c.courseID, c.course_name, s.CRN, s.section_num
+ORDER BY c.course_name, s.section_num;
+'''
+
+    cursor.execute(the_query)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype='application/json'
+    return the_response 
+
+
+@data.route("/data/course", methods=["GET"])
+def get_course():
+    
+    cursor = db.get_db().cursor()
+    the_query= '''
+SELECT c.courseID, c.course_name, c.credits, d.deptName
+FROM Course AS c
+JOIN Department AS d ON c.deptID = d.deptID
+ORDER BY d.deptName, c.course_name;
+'''
+
+    cursor.execute(the_query)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype='application/json'
+    return the_response 
+
+@data.route("/data/course/<int:course_id>", methods=["GET"])
+def get_course_id(course_id):
+    
+    cursor = db.get_db().cursor()
+    the_query= '''
+SELECT
+   c.courseID,
+   c.course_name,
+   d.deptName,
+   COUNT(e.studentID) AS total_enrolled,
+   SUM(e.enrollment_status = 'Completed') AS completed,
+   SUM(e.enrollment_status = 'Dropped') AS dropped,
+   ROUND(SUM(e.enrollment_status = 'Completed') * 100.0 / COUNT(e.studentID), 2) AS completion_rate
+FROM Course AS c
+JOIN Department AS d ON c.deptID = d.deptID
+JOIN Section AS s ON c.courseID = s.CRN
+JOIN Enrollment AS e ON s.CRN = e.CRN
+WHERE c.courseID = %s
+GROUP BY c.courseID, c.course_name, d.deptName;
+'''
+
+    cursor.execute(the_query, (course_id,))
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype='application/json'
+    return the_response 
